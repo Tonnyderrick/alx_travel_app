00eb0a4f875dde4784e1d1a05bfb4d61ff467458
Tonnyderrick <tonnyaketch@gmail.com>
2025-05-31 16:33:53 +0300

    m

---
+++ alx_travel_app/listings/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class ListingSerializer(serializers.ModelSerializer):
+    bookings = BookingSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = ['id', 'name', 'location', 'price', 'description', 'bookings']
